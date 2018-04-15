57a269a10c55d68a1ec8cade4ed7f00e59bc1850
Patrick VanVorce <maholeycowdevelopment@gmail.com>
2018-04-15 15:59:31 -0700

    Initial commit

---
+++ app/src/main/java/com/maholeycowdevelopment/android/purseshooter/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.maholeycowdevelopment.android.purseshooter;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
